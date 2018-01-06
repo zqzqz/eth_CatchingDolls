947ae4ee1de187e2d759d196231912d2a06bf259
zqzqz <443291890@qq.com>
2018-01-06 10:41:03 +0800

    test

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.engine('html', hbs.__express);
-
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(express.logger('dev'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,16 @@
 module.exports = function (app) {
 	
 	app.get('/', function(req, res) {
-		res.render('index', {title:"Catch Your Dolls!"});
+		res.render('index', {title:"Catch Your Dolls!", chances:"0", dolls:"0 0 0 0 0"});
+	});
+
+	app.get('/purchase', function(req, res) {
+		console.log("purchase");
+		res.send("1");
+	});
+
+	app.get('/catch', function(req, res) {
+		console.log("catch");
+		res.send("1 0 0 0 0");
 	});
 };
