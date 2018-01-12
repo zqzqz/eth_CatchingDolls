ce6ed27789071e59e38ca827d801f09c95139f9a
zqzqz <443291890@qq.com>
2018-01-12 17:39:33 +0800

    complete contract designing

--- test/TestTinyGame.js
+++ test/TestTinyGame.js
@@ -3,28 +3,38 @@ var TinyGame = artifacts.require("./TinyGame.sol");
 contract('TinyGame', function(accounts) {
 	
 	var user = accounts[0];
+	var recipicent = accounts[1];
 	var game;
-	
-	it("chances should by added correctly", function() {
-	  return TinyGame.deployed().then(function(instance) {
-            game = instance;
-	    return instance.payToCatch({from: user, value: web3.toWei(1,'ether')});
-	  }).then(function() {
-	    return game.getChancesByAddress.call(user);
-	  }).then(function(c) {
-            assert.equal(c.valueOf(), 1, "error: chances not added");
-	  });
-	});
-
 
 	it("doll should be catched correctly", function() {
 	  return TinyGame.deployed().then(function(instance) {
 	    game = instance;
-            return game.catchDoll(user, 0);
+	    console.log("1");
+            return game.payToCatch.sendTransaction({from:user, value:web3.toWei(1, "ether")});
 	  }).then(function() {
 	    return game.getDollsByAddress.call(user);
 	  }).then(function(c) {
-            assert.equal(c[0].valueOf(), 1, "error: dolls not catched");
+	    console.log(c);
+	  }).then(function() {
+            game.payToCatch.sendTransaction({from:user, value:web3.toWei(1, "ether")});
+	    return game.getDollsByAddress.call(user);
+	  }).then(function(c) {
+	    console.log(c);
+	  })
+	});
+
+	it("transfer dolls from user0 to user1", function() {
+	  return TinyGame.deployed().then(function(instance) {
+	    game = instance;
+            return game.transferDoll.sendTransaction(recipicent, [1,0,0,0,0]);
+	  }).then(function(b) {
+	    console.log(b);
+	    return game.getDollsByAddress.call(user);
+	  }).then(function(c) {
+	    console.log(c);
+	    return game.getDollsByAddress.call(recipicent);
+	  }).then(function(c) {
+	    console.log(c);
 	  });
 	});
 	
